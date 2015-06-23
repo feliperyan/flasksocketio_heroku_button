d71c878c4275499196db5a32fb343dc7bd0f2e11
Felipe Ryan <fryan@fryan-ltm2.internal.salesforce.com>
2015-06-23 17:42:21 +1000

    Can now use curl to issue a POST and push messages to browser

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask.ext.socketio import SocketIO, emit
 import json
 
@@ -6,16 +6,31 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+app.debug = True
 
 @app.route('/')
 def hello():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
+@app.route('/api/v1.0/scans', methods=['POST'])
+def receive_scan_from_api():
+    if not request.json:
+        abort(400)
+    print request.json
+    
+    msg = json.dumps({'message':'Received Scan'})
+    socketio.emit('scan', msg)
+
+    return jsonify({'Response':'All ok'}), 201
+
+
+
+# Actual Socket Events
 @socketio.on('first connect')
 def handle_my_custom_event(jsonMessage):
     print('received json: ' + str(jsonMessage))
-    reply = json.dumps({'body':'You are connected'})
+    reply = json.dumps({'message':'You are connected'})
     emit('pong', reply)
 
 
