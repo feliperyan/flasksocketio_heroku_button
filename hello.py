a309152f0a5efac12f3ec7ea39e701e735235642
Felipe Ryan <fryan@fryan-ltm2.internal.salesforce.com>
2015-06-23 15:30:45 +1000

    Commiting from SF

--- hello.py
+++ hello.py
@@ -1,10 +1,21 @@
-import os
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def hello():
-	return 'Hello'
+	return render_template('index.html')
 
+
+@socketio.on('first connect')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+
+if __name__ == '__main__':
+    socketio.run(app)
 
