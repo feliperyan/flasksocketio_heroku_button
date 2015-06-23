34441a6cb1865f75461e590f283972c964f49f76
Felipe Ryan <feliperyan@Felipes-MacBook-Pro.local>
2015-06-23 19:08:53 +1000

    now outdated home dev

--- hello.py
+++ hello.py
@@ -1,10 +1,24 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask.ext.socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.debug = True
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def hello():
-	return 'Hello'
+	print 'rendering...'
+	return render_template('index.html')
 
 
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+
+if __name__ == '__main__':
+    socketio.run(app)
